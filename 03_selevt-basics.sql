54f5017682f6b9a50ee727c7a96166267abbaf70
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-04-03 16:43:46 -0400

    Create 3rd sql-zoo

---
+++ 03_selevt-basics.sql
@@ -0,0 +1,67 @@
+-- https://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial
+-- 1.
+-- Change the query shown so that it displays Nobel prizes for 1950.
+
+SELECT
+ yr, subject, winner
+  FROM 
+  nobel
+ WHERE 
+ yr = 1950
+
+-- 2.
+-- Show who won the 1962 prize for literature.
+SELECT
+ winner
+  FROM
+   nobel
+ WHERE
+  yr = 1962
+   AND subject
+    = 'literature'
+
+--     3.
+-- Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr,subject  
+FROM nobel
+WHERE  winner='Albert Einstein'
+
+
+-- 4.
+-- Give the name of the 'peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE subject="peace"
+AND yr>=2000
+
+-- 5.
+-- Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive
+
+SELECT yr, subject,winner
+FROM nobel
+WHERE subject='literature'
+AND yr >= 1980
+AND yr <= 1989
+
+
+-- 6.
+-- Show all details of the presidential winners:
+
+-- Theodore Roosevelt
+-- Thomas Woodrow Wilson
+-- Jimmy Carter
+-- Barack Obama
+
+SELECT yr,subject,winner
+FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+'Thomas Woodrow Wilson',
+'Jimmy Carter',
+'Barack Obama')
+
+
+-- 7.
+-- Show the winners with first name John
+SELECT winner
+FROM nobel
+WHERE winner LIKE'John%'
