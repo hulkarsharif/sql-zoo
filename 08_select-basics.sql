ba7d6a230e20f7c9561a1869ba9c072ab5c24091
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-05-14 11:19:27 -0400

    Finish the lesson2 in sql-08

--- 08_select-basics.sql
+++ 08_select-basics.sql
@@ -6,3 +6,10 @@
 SELECT name
 FROM teacher
 WHERE dept IS NULL;
+
+-- 2.
+-- Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
